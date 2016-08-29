3481318a6057df3d56628c197109a62630b96418
rghv96 <raghavsethi96@gmail.com>
2016-08-29 16:01:10 +0530

    9 done

--- main.cpp
+++ main.cpp
@@ -4,6 +4,20 @@ using namespace std;
 int main(){
     print_hello();
     cout<< endl;
-    cout<<"The factorial of 5 is "<< factorial(5)<< endl;
+    cout<<"Enter a 	number whose factorial you want.\n";
+    int n;
+    cin>>n;
+    if(factorial(n) == -1)
+    {
+    	cout<<"Please enter a positive number.\n"
+    	while(n<0)
+    	{
+    		cin>>n;
+    		if(n<0)
+    			cout<<"Please enter a positive number.\n"
+    	}
+
+    }
+    cout<<"The factorial of "<<n<<" is "<< factorial(n)<< endl;
     return 0;
 }
