234b447989c54b08326311e7ef5e0dbdb7e8f44c
priyanshsaxena <geniuspriyansh@gmail.com>
2016-08-29 16:06:12 +0530

    modified main

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 int main(){
     print_hello();
     cout<< endl;
-    cout<<"The factorial of 5 is "<< factorial(5)<< endl;
+    int n = -1;
+    while(n<0) {
+    	cout << "Enter a number: ";
+    	cin >> n;
+    }	
+    cout<<"The factorial of " << n << "is "<< factorial(n)<< endl;
     return 0;
 }
