a9eefd9c2eca1a26c0e5804df69ad9eb6d84bd0c
Harsh Gupta <harshgupta.11dec@gmail.com>
2016-08-29 15:53:55 +0530

    Add check for negative numbers in factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,6 +1,10 @@
 #include "functions.h"
 
 int factorial(int n){
+    while(n <= 0){
+	cout << "\nPlease enter the positive number: ";
+	cin >> n;
+    }	
     if(n!=1){
 	return(n * factorial(n-1));
     }
