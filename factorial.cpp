953c0948826d673fc50578259082877f265c36e9
priyanshsaxena <geniuspriyansh@gmail.com>
2016-08-29 15:53:51 +0530

    added negative factorial check

--- factorial.cpp
+++ factorial.cpp
@@ -1,6 +1,10 @@
 #include "functions.h"
 
 int factorial(int n){
+	if(n<0) {
+		std::cout << "Please enter a positive value. Returning -1.\n";
+		return -1;	
+	}
     if(n!=1){
 	return(n * factorial(n-1));
     }
