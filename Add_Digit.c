8b14ea74c45c9e04d5c648eddec5128ccb720f85
priyanshsaxena <geniuspriyansh@gmail.com>
2016-09-17 22:13:42 +0530

    final commit

--- Add_Digit.c
+++ Add_Digit.c
@@ -2,7 +2,7 @@
  
 int main()
 {
-<<<<<<< HEAD
+
    // declare variables to be used
    int n, sum = 0, remainder;
  
@@ -14,36 +14,16 @@ int main()
    while(n != 0)
    {
       remainder = n % 10;
-	  cin << "The Remainder is " << remainder << "\n";
-=======
-   // declare variables
-   int n, sum = 0, remainder;
-   
-  // take input
-   printf("Enter an integer\n");
-   scanf("%d",&n);
-   
-   // process 
-   while(n != 0)
-   {
-      remainder = n % 10;
       if(remainder<0 && n<=-10) remainder *= -1;
-	  printf("The Remainder is %d\n", remainder);
->>>>>>> old
-      sum = sum + remainder;
+	  cout << "The Remainder is " << remainder << "\n";
+	  sum = sum + remainder;
 	  cout << "The Sum is " << sum << "\n";
       n = n / 10;
 	  cout << "The n is " << n << "\n";
    }
-<<<<<<< HEAD
- 
+    
    // output
    cout << "Sum of digits of entered number = "<< sum << "\n";
-=======
-   
-   // output result 
-   printf("Sum of digits of entered number = %d\n",sum);
->>>>>>> old
- 
+    
    return 0;
 }
